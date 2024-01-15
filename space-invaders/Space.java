aa2bcdf273dd80bc6fdd7695669c25d79b36e89c
Gabriel <gabrielfrnz31@gmail.com>
2024-01-15 09:28:47 +0100

    Update space background

--- space-invaders/Space.java
+++ space-invaders/Space.java
@@ -18,6 +18,33 @@ public class Space extends World
     {    
         // Create a new world with the height and width defined in this class.
         super(width, height, 1); 
+        setBackground();
         addObject(new Spaceship(), width/2, height/8*7);
+    }
+    
+    /**
+     * Set the background of the world.
+     */
+    private void setBackground() {
+        GreenfootImage background = getBackground();
+        background.setColor(Color.BLACK);
+        background.fill();
+        Greenfoot.setSpeed(50);
+        createStars(1000);
+    }
+    
+    /**
+     * Create random stars.
+     * @param number The number of stars.
+     */
+    private void createStars(int number) 
+    {
+        GreenfootImage background = getBackground();             
+        for (int i=0; i < number; i++) {            
+             int x = Greenfoot.getRandomNumber(getWidth());
+             int y = Greenfoot.getRandomNumber(getHeight());
+             int color = 150 - Greenfoot.getRandomNumber(120);
+             background.setColorAt(x, y, new Color(color,color,color));
+        }
     }
 }
