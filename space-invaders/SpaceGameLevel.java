d455fc3d8a1809bacc2f5635726c06f4f6365756
Gabriel <gabrielfrnz31@gmail.com>
2024-01-23 09:20:57 +0100

    Adding SpaceGameLevel enum (fixes running of game)

---
+++ space-invaders/SpaceGameLevel.java
@@ -0,0 +1,12 @@
+/**
+ * Write a description of class SpaceGameLevel here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public enum SpaceGameLevel {
+    BEGINNER,
+    MEDIUM,
+    HARD,
+    PRO
+}
